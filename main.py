c8d63a38a84da0c134e7b57de8d663ee2e94e878
Geovane Leite de Carvalho Filho <geo.vane.20002@gmail.com>
2022-02-01 19:08:02 -0300

    More datasets

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import functions as fct
 import matplotlib.pyplot as plt
-from dataset import usc312, sgb128, wg59
+import dataset
 
 auxList = []
 fig, ax = plt.subplots()
 
-for i in usc312:
+for i in dataset.usc312:
   auxList.append(fct.City(x = i[0], y = i[1]))
   ax.scatter(x = i[0], y = i[1])
 
 plt.show()
-fct.geneticAlgorithmPlot(population=auxList, popSize=500, eliteSize=20, mutationRate=0.01, generations=500)
+fct.geneticAlgorithmPlot(population=auxList, popSize=100, eliteSize=20, mutationRate=0.01, generations=1000)
 
 
