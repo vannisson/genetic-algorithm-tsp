986f7ce479d562eee612a7de09e5eb59d971de49
Geovane Leite de Carvalho Filho <geo.vane.20002@gmail.com>
2022-02-04 17:12:52 -0300

    Project Finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import functions as fct
 import matplotlib.pyplot as plt
 import dataset
+import time
 
 auxList = []
 fig, ax = plt.subplots()
@@ -10,6 +11,7 @@ for i in dataset.usc312:
   ax.scatter(x = i[0], y = i[1])
 
 plt.show()
+inicio = time.time()
 fct.geneticAlgorithmPlot(population=auxList, popSize=100, eliteSize=20, mutationRate=0.01, generations=1000)
-
-
+fim = time.time()
+print(fim - inicio)
